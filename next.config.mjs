436c87eed471eefedef9d04b93d53e8f58575bd4
pseudoparenchymatous <105337186+pseudoparenchymatous@users.noreply.github.com>
2025-09-13 11:15:32 +0000

    Remove config for exporting

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  images: { unoptimized: true },
-  basePath: '/portfolio',
-  assetPrefix: '/portfolio/',
 };
 
 export default nextConfig;
