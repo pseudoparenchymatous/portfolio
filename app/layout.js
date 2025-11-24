7cf2048d35f26442770d8094e942d0136c007ea9
pseudoparenchymatous <105337186+pseudoparenchymatous@users.noreply.github.com>
2025-11-24 22:07:15 +0000

    Use `Manrope` Google font

--- app/layout.js
+++ app/layout.js
@@ -1,13 +1,7 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Manrope } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const manrope = Manrope({
   subsets: ["latin"],
 });
 
@@ -20,7 +14,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className="scroll-smooth">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${manrope.className} antialiased`}
       >
         {children}
       </body>
