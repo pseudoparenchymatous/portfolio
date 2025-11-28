34dc6e5e7157426ec3b15b0d4705a4ce1cb73ed2
pseudoparenchymatous <105337186+pseudoparenchymatous@users.noreply.github.com>
2025-11-28 17:12:43 +0000

    feat: add preloader animation

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,8 +1,161 @@
 "use client";
 
 import { vertexShader, fragmentShader } from "@/lib/shader";
+import gsap from "gsap";
+import { CustomEase } from "gsap/CustomEase";
+import { SplitText } from "gsap/SplitText";
+import { useGSAP } from "@gsap/react";
 import { useEffect, useRef } from "react";
 import * as THREE from "three";
+import { useState } from "react";
+
+gsap.registerPlugin(CustomEase, SplitText);
+CustomEase.create("hop", ".8, 0, .3, 1");
+
+const Preloader = ({ setIsLoading }) => {
+  useGSAP(() => {
+    const counter3 = document.querySelector(".counter-3");
+
+    for (let i = 0; i < 2; i++) {
+      for (let j = 0; j < 10; j++) {
+        const div = document.createElement("div");
+        div.className = "num";
+        div.textContent = j;
+        counter3.appendChild(div);
+      }
+    }
+
+    const finalDiv = document.createElement("div");
+    finalDiv.className = "num";
+    finalDiv.textContent = "0";
+    counter3.appendChild(finalDiv);
+
+    function animate(counter, duration, delay = 0) {
+      const numHeight = counter.querySelector(".num").clientHeight;
+      const totalDistance = (counter.querySelectorAll(".num").length - 1) * numHeight;
+
+      gsap.to(counter, {
+        y: -totalDistance,
+        duration: duration,
+        delay: delay,
+        ease: "power2.inOut",
+      });
+    }
+
+    animate(counter3, 5);
+    animate(document.querySelector(".counter-2"), 6);
+    animate(document.querySelector(".counter-1"), 2, 4);
+
+    let nameSplit = SplitText.create(".my-name", {
+      type: "words",
+      mask: "words",
+    });
+
+    let titleSplit = SplitText.create(".my-title", {
+      type: "words",
+      mask: "words",
+    });
+
+    const tl = gsap.timeline({
+      defaults: { ease: "hop" },
+      onComplete: () => {
+        document.getElementById("preloader").style.display = "none";
+        document.getElementById("preloaderOverlay").style.display = "none";
+      }
+    });
+    tl.to("#heroSection", {
+      clipPath: "polygon(0 48%, 100% 48%, 100% 52%, 0 52%)",
+      duration: 1,
+    }, 6)
+      .set(".preloader", {
+        clipPath: "polygon(0 0, 100% 0, 100% 50%, 0 50%)",
+      }, 7)
+      .set(".preloader-overlay", {
+        clipPath: "polygon(0 50%, 100% 50%, 100% 100%, 0 100%)",
+      }, 7)
+      .to( [".preloader", ".preloader-overlay"], {
+        y: (i) => (i === 0 ? "-50%" : "50%"),
+        duration: 1,
+      }, 7)
+      .to("#heroSection", {
+        clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+        duration: 1,
+      }, 7)
+      .from(nameSplit.words, {
+        y: 100,
+        stagger: 0.05,
+      }, 8).
+      from(titleSplit.words, {
+        y: 100,
+        stagger: 0.05,
+      }, 8);
+
+    // If I want to hide the numbers before the reveal
+    // gsap.to(".digit", {
+    //   top: -150,
+    //   stagger: {
+    //     amount: 0.25,
+    //   },
+    //   delay: 6,
+    //   duration: 1,
+    //   ease: "power4.inOut",
+    // });
+
+
+  }, []);
+
+  return (
+    <>
+      <div id="preloader" className="preloader fixed w-screen h-svh bg-zinc-900 z-2">
+        <div className="counter fixed top-1/2 left-1/2 -translate-1/2 flex h-[100px] text-[100px]/[102px] pointer-events-none">
+          <div className="counter-1">
+            <div className="num">0</div>
+            <div className="num num1-offset1 relative right-[-25px]">1</div>
+          </div>
+          <div className="counter-2">
+            <div className="num">0</div>
+            <div className="num num1-offset2 relative right-[-25px]">1</div>
+            <div className="num">2</div>
+            <div className="num">3</div>
+            <div className="num">4</div>
+            <div className="num">5</div>
+            <div className="num">6</div>
+            <div className="num">7</div>
+            <div className="num">8</div>
+            <div className="num">9</div>
+            <div className="num">0</div>
+          </div>
+          <div className="counter-3">
+            <div className="num">0</div>
+            <div className="num">1</div>
+            <div className="num">2</div>
+            <div className="num">3</div>
+            <div className="num">4</div>
+            <div className="num">5</div>
+            <div className="num">6</div>
+            <div className="num">7</div>
+            <div className="num">8</div>
+            <div className="num">9</div>
+          </div>
+        </div>
+      </div>
+      <div id="preloaderOverlay" className="preloader-overlay fixed w-screen h-dvh bg-zinc-900 z-1">
+        <div className="counter fixed top-1/2 left-1/2 -translate-1/2 flex h-[100px] text-[100px]/[102px] pointer-events-none">
+          <div className="">
+            <div className="relative right-[-25px]">1</div>
+            <div className="">0</div>
+          </div>
+          <div className="">
+            <div className="relative">0</div>
+          </div>
+          <div className="">
+            <div className="num">0</div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default function Hero() {
   const sectionRef = useRef(null);
@@ -108,22 +261,25 @@ export default function Hero() {
   }, []);
 
   return (
-    <section
-      ref={sectionRef}
-      id="heroSection"
-      className="relative w-full h-svh overflow-hidden pointer-events-none"
-    >
-      <img 
-        ref={textureImageRef}
-        id="glassTexture"
-        src="hero.jpg"
-        className="hidden"
-        alt="Hero image"
-      />
-      <div id="heroContent" className="text-white absolute left-0 bottom-0 w-full flex flex-col-reverse p-10 md:flex-row md:justify-between md:items-end">
-        <h1 className="text-7xl">Georey S. Saliente</h1>
-        <p>Software Developer</p>
-      </div>
-    </section>
+    <>
+      <Preloader />
+      <section
+        ref={sectionRef}
+        id="heroSection"
+        className="relative w-full h-svh overflow-hidden pointer-events-none z-2"
+      >
+        <img 
+          ref={textureImageRef}
+          id="glassTexture"
+          src="hero.jpg"
+          className="hidden"
+          alt="Hero image"
+        />
+        <div id="heroContent" className="text-white absolute left-0 bottom-0 w-full flex flex-col-reverse p-10 md:flex-row md:justify-between md:items-end">
+          <h1 className="my-name text-7xl">Georey S. Saliente</h1>
+          <p className="my-title">Software Developer</p>
+        </div>
+      </section>
+    </>
   );
 }
